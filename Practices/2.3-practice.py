a45bb22a82a90c09d441210ffda0faaa99ac6ead
Paynter Gabriel <826999@stma.k12.mn.us>
2025-09-25 13:15:23 -0500

    message

---
+++ Practices/2.3-practice.py
@@ -0,0 +1,18 @@
+#String - "in quotations"
+# Integer - a whole number ex. 1 4 -10 etc.
+#Float - a decimal number ex 3.1415678347
+# Character - a single glyph ex. 'c'
+# Bool - true or false 
+
+# How to define a variable!!
+# name = value
+
+x=10
+
+print(x)
+
+x = 10
+y = 50
+potato = 7
+
+print(potato + x)
