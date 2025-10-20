d2070e9a27189993f6db617128e9b067ef0ef609
Paynter Gabriel <826999@stma.k12.mn.us>
2025-10-20 13:20:39 -0500

    Hurricane Classifier

---
+++ Practices/stringprac.py
@@ -0,0 +1,9 @@
+sentence = input("Enter a sentence: ")
+
+print("Uppercase:", sentence.upper())
+
+print("Trimmed:", sentence.strip())
+
+print("Replaced:", sentence.replace("bad", "good"))
+
+print("Ends with a period:", sentence.endswith("."))
